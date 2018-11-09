97a36d06a478cfed8998eacb883d031f3d8987d5
nada.alaskari <nada.alaskari@hotmail.com>
2018-11-09 16:32:41 +0200

    1st Version of ElMenus Task

---
+++ Task/src/test/java/TestCases/TC01_OpenGameCateogry.java
@@ -0,0 +1,23 @@
+package TestCases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import Core.Core;
+import Pages.WelcomeScreen;
+
+public class TC01_OpenGameCateogry extends Core{
+	
+	
+	@Test
+	public void test_001_Welcome_Screen() throws Exception
+	{
+	   
+	    WelcomeScreen welcome =PageFactory.initElements(driver, WelcomeScreen.class);
+		welcome.addGameToCart();
+		log.debug("Game Added to the Cart");
+		
+		
+	}
+
+}
---
+++ Task/src/test/java/Utilities/ReportHelper.java
@@ -0,0 +1,31 @@
+package Utilities;
+
+
+
+import java.io.File;
+
+import com.relevantcodes.extentreports.DisplayOrder;
+import com.relevantcodes.extentreports.ExtentReports;
+
+public class ReportHelper {
+	private static ExtentReports report;
+	
+	public static ExtentReports reportinit()
+	{
+		if (report==null)
+		{
+		
+			report= new ExtentReports(System.getProperty("user.dir")+"\\src\\test\\resources\\Reports\\Extentetreport.html", true, DisplayOrder.OLDEST_FIRST);
+		  report.loadConfig(new File (System.getProperty("user.dir")+"\\extent-config.xml"));
+		}
+		
+		
+		return report;
+		
+		
+		
+		
+	}
+	
+
+}
